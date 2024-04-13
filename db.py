4077f1493cd1198e75f8fc7daf271b80ef1d1fb3
Alexander Yoo <95768413+alexanderkyoo@users.noreply.github.com>
2024-04-13 13:49:08 -0400

    sql

--- db.py
+++ db.py
@@ -41,9 +41,12 @@ class Item(Base):
 
 
 load_dotenv()
+db_user = os.getenv('DB_USER')
 db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
 
-_engine = sqlalchemy.create_engine(f'postgresql+psycopg2://postgres:{db_password}@localhost/Gubble')
+engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_name}')
 
 #-----------------------------------------------------------------------
 # Database get methods
