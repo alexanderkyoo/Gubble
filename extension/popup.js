c3987ac6137b4c029f875cf22ae2ea3364835c6e
Alexander Yoo <95768413+alexanderkyoo@users.noreply.github.com>
2024-04-14 15:19:40 -0400

    inventory page

--- extension/popup.js
+++ extension/popup.js
@@ -2,5 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('redirect').addEventListener('click', function() {
         chrome.tabs.create({url: "https://gubble.onrender.com/"});
     });
-});
 
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+        if (chrome.runtime.lastError) {
+            console.log(chrome.runtime.lastError.message);
+        } else {
+            console.log('Token acquired:', token);
+            // You can use this token to authenticate the user.
+        }
+    });
+});
