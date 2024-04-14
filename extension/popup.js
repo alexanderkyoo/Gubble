7ab5d9a1e23654e1cec0304fd7feee8891ebdddb
Alexander Yoo <95768413+alexanderkyoo@users.noreply.github.com>
2024-04-13 20:55:40 -0400

    bubbles

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('redirect').addEventListener('click', function() {
-        chrome.tabs.create({url: "gubble.onrender.com"});
+        chrome.tabs.create({url: "https://gubble.onrender.com/"});
     });
 });
 
