99d3ee5664603ec193817bb560b2b3cbb7eb1ee6
Alexander Yoo <95768413+alexanderkyoo@users.noreply.github.com>
2024-04-22 00:00:13 -0400

    everting

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('redirect').addEventListener('click', function() {
-        chrome.tabs.create({url: "https://gubble.onrender.com/"});
-    });
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.getElementById('redirect').addEventListener('click', function() {
+//         chrome.tabs.create({url: "https://gubble.onrender.com/"});
+//     });
 
-    chrome.identity.getAuthToken({interactive: true}, function(token) {
-        if (chrome.runtime.lastError) {
-            console.log(chrome.runtime.lastError.message);
-        } else {
-            console.log('Token acquired:', token);
-            // You can use this token to authenticate the user.
-        }
-    });
-});
+//     chrome.identity.getAuthToken({interactive: true}, function(token) {
+//         if (chrome.runtime.lastError) {
+//             console.log(chrome.runtime.lastError.message);
+//         } else {
+//             console.log('Token acquired:', token);
+//             // You can use this token to authenticate the user.
+//         }
+//     });
+// });
 
+fetch('http://localhost:3000/get-data')
+    .then(response => response.json())
+    .then(data => {
+        // Select the element and display the data
+        const dataElement = document.querySelector('#data');
+        dataElement.textContent = JSON.stringify(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
---
+++ extension/server-side/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const { spawn } = require('child_process');
+const app = express();
+const port = 3000;
+
+app.get('/get-data', (req, res) => {
+    const python = spawn('python3', ['.../db.py']);
+    let dataToSend;
+
+    python.stdout.on('data', (data) => {
+        console.log('Pipe data from python script ...');
+        dataToSend = data.toString();
+    });
+
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://klnldgenohnbnfddnccjehnhmnbfkchp');
+        res.json({ message: dataToSend });
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
