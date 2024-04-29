76672597e973f34473072b0f0838689e0d2cc907
Alexander Yoo <95768413+alexanderkyoo@users.noreply.github.com>
2024-04-28 20:46:12 -0400

    reciept

--- gubble.py
+++ gubble.py
@@ -124,16 +124,16 @@ def recieptscanner():
         text = reciept.parse_text_from_image(photo_data)
         print(text)
         if text != '':
-            standardized = reciept.analyze_text(text)
-            #print(standardized_text)
-            #items = standardized_text.split('\n')
-            for item in standardized:
+            standardized_text = reciept.analyze_text(text)
+            print(standardized_text)
+            items = standardized_text.split('\n')
+            for item in items:
                 item_info = {
-                    'item_name': item['item_name'],
+                    'item_name': item.split(',')[0].split(': ')[1],
                     'inventory_id': flask.session['inventory_id'],
-                    'category_id': db.retrieveOrInsertCategory('', item['item_name']),
+                    'category_id': db.retrieveOrInsertCategory('', item.split(',')[0].split(': ')[1]),
                     'description': '',
-                    'quantity': item['quantity']
+                    'quantity': item.split(',')[1].split(': ')[1]
                 }
                 db.insertItem(item_info)
         return flask.redirect(flask.url_for('inventory')) 
